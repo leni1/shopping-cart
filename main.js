81e0e31faf29d9298ddde8b80e21bf23e4560096
Leni Kadali Mutungi <leni1@users.noreply.github.com>
2022-11-07 23:18:19 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,93 @@
+let shop = document.getElementById("shop");
+
+let shopItemsData = [
+  {
+    id: "item-one",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "A casual shirt you can wear on any occasion",
+    img: "images/img-1.jpg"
+  },
+  {
+    id: "item-two",
+    name: "Office Shirt",
+    price: 45,
+    desc: "A office shirt you can wear comfortably to work",
+    img: "images/img-2.jpg"
+    
+  },
+  {
+    id: "item-three",
+    name: "T-Shirt",
+    price: 25,
+    desc: "A cool tee to express your personality",
+    img: "images/img-3.jpg"
+  },
+  {
+    id: "item-four",
+    name: "Men's Suit",
+    price: 300,
+    desc: "A classic suit for a timeless fashion",
+    img: "images/img-4.jpg"
+  }
+]
+
+let basket = [{
+}];
+
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData.map((x) => {
+    let { id, name, price, desc, img } = x
+    return `
+    <div id="product-id-${id}" class="item">
+      <img width="220" src="${img}" alt="">
+      <div class="details">
+        <h3>${name}</h3>
+        <p>${desc}</p>
+        <div class="price-quantity">
+          <h2>$ ${price}</h2>
+          <div class="buttons">
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id=${id} class="quantity">0</div>
+            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  }).join(""));
+};
+
+generateShop();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id)
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item +=1;
+  }
+  update(selectedItem.id);
+};
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id)
+
+  if (search === 0) return;
+  else {
+    search.item -=1;
+  }
+  update(selectedItem.id);
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id)
+  document.getElementById(id).innerHTML = search.item;
+};
